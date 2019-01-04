7bbfcc079f54257085896dc4edd15cf7f2a18524
Gilberto Pineda <gilbertopineda.work@gmail.com>
2019-01-04 11:28:39 -0400

    11 of Test Passed

--- ChefManager/IngredientInfo.cs
+++ ChefManager/IngredientInfo.cs
@@ -69,6 +69,7 @@ namespace ChefManager
                   MeasurementUnit = fReader.ReadLine(),
                   Description = fReader.ReadToEnd()
                   });
+                   
                } 
             }
         }
